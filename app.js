5101fcbc7d3673f779fc429b3e7c7a8eaa70ff50
Tom D <dunerz@hotmail.com>
2020-12-15 15:28:56 +1100

    Setup basic Express app with with test and mongoose dependencies

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const artworkRouter = require('./routes/artwork_routes');
+
+const port = process.env.port || 3009;
+
+const app = express();
+
+// Connecting to database
+const dbConn = 'mongodb://localhost/blog_app'
+// Set three properties to avoid deprecation warnings:
+// useNewUrlParser: true
+// useUnifiedTopology: true
+// useFileAndModify: false
+mongoose.connect(dbConn, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+},
+(err) => {
+  if (err) {
+    console.log('Error connecting to database', err);
+  } else {
+    console.log('Connected to database!');
+  }
+});
+
+// Routes
+app.use('/artworks', artworkRouter);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
